153d8532891d4b1f0bfe2ed6ffce16eeabe320b8
monishass <monishasakthivelu@gmail.com>
2021-03-15 00:05:15 +0530

    added_models_services_and_keys_in_env_file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  API_URL: 'https://demo-api.now.sh/users',
+  production: false,
 };
 
 /*
