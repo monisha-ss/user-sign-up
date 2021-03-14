a195c5a665bea400f658a8c950a2e52a21483b05
monishass <monishasakthivelu@gmail.com>
2021-03-14 23:56:44 +0530

    added_lazy_loading_and_sign_ip_component

---
+++ src/app/sign-up/sign-up.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+
+
+@NgModule({
+  declarations: [SignUpComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SignUpModule { }
